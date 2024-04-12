945b2c86f3ca847e6b186b3a0afb4f7cdd3f5281
Wl.se <muhammad.wildan@raharja.info>
2024-04-12 19:15:55 +0700

    update content

--- js/main.js
+++ js/main.js
@@ -4,3 +4,39 @@ const navbarMenu = document.querySelector("#navbar");
 hamburgerMenu.addEventListener("click", () => {
   navbarMenu.classList.toggle("hidden");
 });
+
+const words = ["Halo, Muhammad Wildan!", "Selamat datang di website anda."];
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type() {
+  currentWord = words[i];
+  if (isDeleting) {
+    document.getElementById("typewriter").textContent = currentWord.substring(
+      0,
+      j - 1
+    );
+    j--;
+    if (j == 0) {
+      isDeleting = false;
+      i++;
+      if (i == words.length) {
+        i = 0;
+      }
+    }
+  } else {
+    document.getElementById("typewriter").textContent = currentWord.substring(
+      0,
+      j + 1
+    );
+    j++;
+    if (j == currentWord.length) {
+      isDeleting = true;
+    }
+  }
+  setTimeout(type, 100);
+}
+
+type();
